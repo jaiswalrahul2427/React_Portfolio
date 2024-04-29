effb6d9597a338d04fe50d63766062cb34484e66
jaiswalrahul2427 <jaiswalrahul2427@gamil.com>
2024-04-29 17:11:59 +0530

    Second Page

--- src/component/SecondPage.jsx
+++ src/component/SecondPage.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import pic2 from '../assets/pic2.png'
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 function SecondPage() {
   return (
-    <div className='min-h-screen '>
-        <div>
-            <img src={pic2} alt="Photo" />
+    <div className='flex flex-wrap justify-between min-h-screen pt-48 mx-5 '>
+      <div className=' lg:w-1/4'>
+        <img src={pic2} alt="Photo" className='rounded-full ' />
         </div>
-        <div>
-            <h1>MY BIOGRAPHY</h1>
-            <h2>Aspiring Computer Science Student and Software Engineer.</h2>
-            <p>Currently, I am immersed in my Bachelor of Computer Applications journey at Dr. Ram Manohar Lohia Avadh University. Throughout my academic pursuits, I have delved into a diverse array of programming languages and technologies, refining my skills in C, C++, Python, JAVA, JAVASCRIPT, TAILWIND CSS, SQL, CSS, HTML, React, Node JS, Express, Mongodb, Redux  and Data Structures and Algorithms</p>
-        <p><span>Name:</span>Rahul Jaiswal</p>
-        <p><span>Email:</span>jaiswalrahul2427@gmail.com</p>
-        <p><span>From:</span>U.P,India</p>
-        <p><span>Follow Me On</span> <a href="https://www.linkedin.com/in/rahul-jaiswal-278079204"><i id="linkedin" className="fa fa-linkedin"></i></a>
-                        <a href="https://instagram.com/jaiswalrahul2427?igshid=MzRlODBiNWFlZA=="> <i id="instagram" className="fa fa-instagram"></i></a>
-                        <a href="https://github.com/jaiswalrahul2427"><i id="github" className="fa fa-github"></i></a></p>
-        </div>
-      
+      <div className='leading-6 lg:w-2/3 font-poppins -pt-10'>
+        <h1 className='text-xl font-kalam '>MY BIOGRAPHY</h1>
+        <h2 className='text-3xl ' >Aspiring Computer Science Student and Software Engineer.</h2>
+        <p className='pt-3 font-thin font-poppins'>Currently, I am immersed in my Bachelor of Computer Applications journey at Dr. Ram Manohar Lohia Avadh University. Throughout my academic pursuits, I have delved into a diverse array of programming languages and technologies, refining my skills in C, C++, Python, JAVA, JAVASCRIPT, TAILWIND CSS, SQL, CSS, HTML, React, Node JS, Express, Mongodb, Redux  and Data Structures and Algorithms</p>
+        <p className='pt-3 '><span className='pt-3 text-xl - '>Name:</span> Rahul Jaiswal</p>
+        <p className='pt-1'><span className='text-xl - font-poppins'>Email:</span> jaiswalrahul2427@gmail.com</p>
+        <p className='pt-1 '><span className='text-xl font-poppins'>From:</span> U.P,India</p>
+
+        <p className='flex pt-1 '><span className='font-bold font-poppins'>Follow Me On: </span>
+<div className='flex space-x-2 text-2xl text-blue-800'>
+<a className='pl-2 ' href="https://www.linkedin.com/in/rahul-jaiswal-278079204"> <FaLinkedin /></a>
+          <a href="https://instagram.com/jaiswalrahul2427?igshid=MzRlODBiNWFlZA=="><FaInstagram /></a>
+          <a href="https://github.com/jaiswalrahul2427"><FaGithub /></a>
+</div>
+          </p>
+     
+      </div>
+
     </div>
   )
 }
